0fbd44b5009f3cfa4a17e5c8f1912a5d695a4f47
Yaroslav <yaroslav.bogdanov.2005@mail.ru>
2025-03-20 16:55:19 +0500

    Подключение БД

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
-import 'auth_signin_widget.dart';
-import 'auth_signup_widget.dart';
-import 'terms_of_service_page.dart';
-import 'privacy_policy_page.dart';
-import 'photo_page.dart';
-import 'exercise_camera_page.dart';
+import 'pages/auth_signin_widget.dart';
+import 'pages/auth_signup_widget.dart';
+import 'pages/terms_of_service_page.dart';
+import 'pages/privacy_policy_page.dart';
+import 'pages/photo_page.dart';
+import 'pages/exercise_camera_page.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  // подключение базы данных
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://wlinnnojuqxbpoeizyvz.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndsaW5ubm9qdXF4YnBvZWl6eXZ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIyMjE5MTQsImV4cCI6MjA1Nzc5NzkxNH0.-rw_YKxF-IWKWyADQaRdrBZqbxa4REO6TrAiBFmAd50',
+  );
+
   runApp(const MyApp());
 }
 
