04fea9080b5e2086c45f7ead016afd262748f775
Yaroslav <yaroslav.bogdanov.2005@mail.ru>
2025-04-05 16:06:34 +0500

    КТ 1

---
+++ lib/exercise_sections/face_exercises/face_2.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Face2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Упражнение 2")),
+      body: Center(
+        child: Text("Страница Face2", style: TextStyle(fontSize: 24)),
+      ),
+    );
+  }
+}
---
+++ lib/exercise_sections/tongue_exercises/tongue_3.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Tongue3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Упражнение 3")),
+      body: Center(
+        child: Text("Страница Tongue3", style: TextStyle(fontSize: 24)),
+      ),
+    );
+  }
+}
