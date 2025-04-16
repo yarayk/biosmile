bcf7ed3b402b53b49c75b8420a42526eeca347e0
Ruslan Shebalin <shebalinrus@gmail.com>
2025-04-16 20:14:20 +0500

    внес правки

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,6 @@ import 'exercise_sections/additional_exercises/additional_3.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'config.dart';
-//final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 void main() async{
   // подключение базы данных
@@ -48,16 +47,6 @@ void main() async{
     anonKey: ANON_KEY,
   );
 
-  // Добавляем подписку на вход пользователя (в том числе через Google)
-  /*Supabase.instance.client.auth.onAuthStateChange.listen((data) {
-    final AuthChangeEvent event = data.event;
-    final session = data.session;
-
-    if (event == AuthChangeEvent.signedIn && session != null) {
-      navigatorKey.currentState?.pushReplacementNamed('/home');
-    }
-  });
-*/
   runApp(const MyApp());
 }
 
