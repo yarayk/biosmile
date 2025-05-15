9253d69943bfaa6a7ed0bd8c1d3965f95bb6d236
VovaArhi0703 <vovaarhi.0703@gmail.com>
2025-05-15 21:40:06 +0500

    Yesssssssss

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'pages/terms_of_service_page.dart';
 import 'pages/privacy_policy_page.dart';
 import 'pages/email_verification_page.dart';
 import 'pages/forgot_password_page.dart';
+import 'pages/password_page.dart';
 import 'pages/reset_password_page.dart';
 import 'pages/home_page.dart';
 import 'pages/exercise_sections.dart';
@@ -16,9 +17,17 @@ import 'exercise_sections/lips_exercises.dart';
 import 'exercise_sections/lips_exercises/lips_1.dart';
 import 'exercise_sections/lips_exercises/lips_2.dart';
 import 'exercise_sections/lips_exercises/lips_3.dart';
+import 'exercise_sections/lips_exercises/lips_4.dart';
+import 'exercise_sections/lips_exercises/lips_5.dart';
+import 'exercise_sections/lips_exercises/lips_6.dart';
+import 'exercise_sections/lips_exercises/lips_7.dart';
 import 'exercise_sections/tongue_exercises/tongue_1.dart';
 import 'exercise_sections/tongue_exercises/tongue_2.dart';
 import 'exercise_sections/tongue_exercises/tongue_3.dart';
+import 'exercise_sections/tongue_exercises/tongue_4.dart';
+import 'exercise_sections/tongue_exercises/tongue_5.dart';
+import 'exercise_sections/tongue_exercises/tongue_6.dart';
+import 'exercise_sections/tongue_exercises/tongue_7.dart';
 import 'exercise_sections/jaw_exercises.dart';
 import 'exercise_sections/jaw_exercises/jaw_1.dart';
 import 'exercise_sections/jaw_exercises/jaw_2.dart';
@@ -27,21 +36,77 @@ import 'exercise_sections/face_exercises.dart';
 import 'exercise_sections/face_exercises/face_1.dart';
 import 'exercise_sections/face_exercises/face_2.dart';
 import 'exercise_sections/face_exercises/face_3.dart';
+import 'exercise_sections/face_exercises/face_4.dart';
+import 'exercise_sections/face_exercises/face_5.dart';
+import 'exercise_sections/face_exercises/face_6.dart';
+import 'exercise_sections/face_exercises/face_7.dart';
+import 'exercise_sections/face_exercises/face_8.dart';
+import 'exercise_sections/face_exercises/face_9.dart';
+import 'exercise_sections/face_exercises/face_10.dart';
+import 'exercise_sections/face_exercises/face_11.dart';
+import 'exercise_sections/face_exercises/face_12.dart';
+import 'exercise_sections/face_exercises/face_13.dart';
+import 'exercise_sections/face_exercises/face_14.dart';
+import 'exercise_sections/face_exercises/face_15.dart';
 import 'exercise_sections/cheeks_exercises.dart';
 import 'exercise_sections/cheeks_exercises/cheeks_1.dart';
 import 'exercise_sections/cheeks_exercises/cheeks_2.dart';
 import 'exercise_sections/cheeks_exercises/cheeks_3.dart';
+import 'exercise_sections/cheeks_exercises/cheeks_4.dart';
+import 'exercise_sections/cheeks_exercises/cheeks_5.dart';
 import 'exercise_sections/additional_exercises.dart';
 import 'exercise_sections/additional_exercises/additional_1.dart';
 import 'exercise_sections/additional_exercises/additional_2.dart';
 import 'exercise_sections/additional_exercises/additional_3.dart';
+import 'exercise_sections/additional_exercises/additional_4.dart';
+
+import 'exercise_sections/face_exercises/face_1_exercises.dart';
+import 'exercise_sections/face_exercises/face_2_exercises.dart';
+import 'exercise_sections/face_exercises/face_3_exercises.dart';
+import 'exercise_sections/face_exercises/face_4_exercises.dart';
+import 'exercise_sections/face_exercises/face_5_exercises.dart';
+import 'exercise_sections/face_exercises/face_6_exercises.dart';
+import 'exercise_sections/face_exercises/face_7_exercises.dart';
+import 'exercise_sections/face_exercises/face_8_exercises.dart';
+import 'exercise_sections/face_exercises/face_9_exercises.dart';
+import 'exercise_sections/face_exercises/face_10_exercises.dart';
+import 'exercise_sections/face_exercises/face_11_exercises.dart';
+import 'exercise_sections/face_exercises/face_12_exercises.dart';
+import 'exercise_sections/face_exercises/face_13_exercises.dart';
+import 'exercise_sections/face_exercises/face_14_exercises.dart';
+import 'exercise_sections/face_exercises/face_15_exercises.dart';
+import 'exercise_sections/jaw_exercises/jaw_1_exercises.dart';
+import 'exercise_sections/jaw_exercises/jaw_2_exercises.dart';
+import 'exercise_sections/jaw_exercises/jaw_3_exercises.dart';
+import 'exercise_sections/additional_exercises/additional_1_exercises.dart';
+import 'exercise_sections/additional_exercises/additional_2_exercises.dart';
+import 'exercise_sections/additional_exercises/additional_3_exercises.dart';
+import 'exercise_sections/additional_exercises/additional_4_exercises.dart';
+import 'exercise_sections/cheeks_exercises/cheeks_1_exercises.dart';
+import 'exercise_sections/cheeks_exercises/cheeks_2_exercises.dart';
+import 'exercise_sections/cheeks_exercises/cheeks_3_exercises.dart';
+import 'exercise_sections/cheeks_exercises/cheeks_4_exercises.dart';
+import 'exercise_sections/cheeks_exercises/cheeks_5_exercises.dart';
+import 'exercise_sections/lips_exercises/lips_1_exercises.dart';
+import 'exercise_sections/lips_exercises/lips_2_exercises.dart';
+import 'exercise_sections/lips_exercises/lips_3_exercises.dart';
+import 'exercise_sections/lips_exercises/lips_4_exercises.dart';
+import 'exercise_sections/lips_exercises/lips_5_exercises.dart';
+import 'exercise_sections/lips_exercises/lips_6_exercises.dart';
+import 'exercise_sections/lips_exercises/lips_7_exercises.dart';
+import 'exercise_sections/tongue_exercises/tongue_1_exercises.dart';
+import 'exercise_sections/tongue_exercises/tongue_2_exercises.dart';
+import 'exercise_sections/tongue_exercises/tongue_3_exercises.dart';
+import 'exercise_sections/tongue_exercises/tongue_4_exercises.dart';
+import 'exercise_sections/tongue_exercises/tongue_5_exercises.dart';
+import 'exercise_sections/tongue_exercises/tongue_6_exercises.dart';
+import 'exercise_sections/tongue_exercises/tongue_7_exercises.dart';
 
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'config.dart';
 import 'package:intl/date_symbol_data_local.dart'; // Для локализации календаря
 import 'deep_link_handler.dart';
-
 
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 // контрольная точка 1
@@ -54,6 +119,20 @@ void main() async{
     anonKey: ANON_KEY,
   );
   await initializeDateFormatting('ru_RU', null); // Инициализация локализации для календаря
+
+  // Обработка deep link (восстановление пароля)
+  Supabase.instance.client.auth.onAuthStateChange.listen((data) async {
+    final AuthChangeEvent event = data.event;
+    final Session? session = data.session;
+
+    if (event == AuthChangeEvent.passwordRecovery && session != null) {
+      navigatorKey.currentState?.pushNamedAndRemoveUntil(
+        '/password-page',
+            (route) => false,
+      );
+    }
+  });
+
   runApp(const MyApp());
 }
 
@@ -65,6 +144,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'My App',
       debugShowCheckedModeBanner: false,
+      navigatorKey: navigatorKey,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
@@ -77,6 +157,7 @@ class MyApp extends StatelessWidget {
         '/privacy': (context) => PrivacyPolicyPage(),
         '/email-verification': (context) => EmailVerificationPage(),
         '/forgot-password': (context) => ForgotPasswordPage(),
+        '/password-page': (context) => ChangePasswordPage(),
         '/reset-password': (context) => ResetPasswordPage(),
         '/home': (context) => HomePage(),
         '/exercise_sections': (context) => ExerciseSectionsPage(),
@@ -86,11 +167,19 @@ class MyApp extends StatelessWidget {
         '/tongue_1': (context) => Tongue1(),
         '/tongue_2': (context) => Tongue2(),
         '/tongue_3': (context) => Tongue3(),
+        '/tongue_4': (context) => Tongue4(),
+        '/tongue_5': (context) => Tongue5(),
+        '/tongue_6': (context) => Tongue6(),
+        '/tongue_7': (context) => Tongue7(),
         '/tongue_exercises': (context) => TongueExercisesPage(),
         '/lips_exercises': (context) => LipsExercisesPage(),
         '/lips_1': (context) => Lips1(),
         '/lips_2': (context) => Lips2(),
         '/lips_3': (context) => Lips3(),
+        '/lips_4': (context) => Lips4(),
+        '/lips_5': (context) => Lips5(),
+        '/lips_6': (context) => Lips6(),
+        '/lips_7': (context) => Lips7(),
         '/jaw_exercises': (context) => JawExercisesPage(),
         '/jaw_1': (context) => Jaw1(),
         '/jaw_2': (context) => Jaw2(),
@@ -99,15 +188,70 @@ class MyApp extends StatelessWidget {
         '/face_1': (context) => Face1(),
         '/face_2': (context) => Face2(),
         '/face_3': (context) => Face3(),
+        '/face_4': (context) => Face4(),
+        '/face_5': (context) => Face5(),
+        '/face_6': (context) => Face6(),
+        '/face_7': (context) => Face7(),
+        '/face_8': (context) => Face8(),
+        '/face_9': (context) => Face9(),
+        '/face_10': (context) => Face10(),
+        '/face_11': (context) => Face11(),
+        '/face_12': (context) => Face12(),
+        '/face_13': (context) => Face13(),
+        '/face_14': (context) => Face14(),
+        '/face_15': (context) => Face15(),
         '/cheeks_exercises': (context) => CheeksExercisesPage(),
         '/cheeks_1': (context) => Cheeks1(),
         '/cheeks_2': (context) => Cheeks2(),
         '/cheeks_3': (context) => Cheeks3(),
+        '/cheeks_5': (context) => Cheeks4(),
+        '/cheeks_4': (context) => Cheeks5(),
         '/additional_exercises': (context) => AdditionalExercisesPage(),
         '/additional_1': (context) => Additional1(),
         '/additional_2': (context) => Additional2(),
         '/additional_3': (context) => Additional3(),
-
+        '/additional_4': (context) => Additional4(),
+        '/face_1_exercises': (context) => Face1Exercises(),
+        '/face_2_exercises': (context) => Face2Exercises(),
+        '/face_3_exercises': (context) => Face3Exercises(),
+        '/face_4_exercises': (context) => Face4Exercises(),
+        '/face_5_exercises': (context) => Face5Exercises(),
+        '/face_6_exercises': (context) => Face6Exercises(),
+        '/face_7_exercises': (context) => Face7Exercises(),
+        '/face_8_exercises': (context) => Face8Exercises(),
+        '/face_9_exercises': (context) => Face9Exercises(),
+        '/face_10_exercises': (context) => Face10Exercises(),
+        '/face_11_exercises': (context) => Face11Exercises(),
+        '/face_12_exercises': (context) => Face12Exercises(),
+        '/face_13_exercises': (context) => Face13Exercises(),
+        '/face_14_exercises': (context) => Face14Exercises(),
+        '/face_15_exercises': (context) => Face15Exercises(),
+        '/jaw_1_exercises': (context) => Jaw1Exercises(),
+        '/jaw_2_exercises': (context) => Jaw2Exercises(),
+        '/jaw_3_exercises': (context) => Jaw3Exercises(),
+        '/additional_1_exercises': (context) => Additional1Exercises(),
+        '/additional_2_exercises': (context) => Additional2Exercises(),
+        '/additional_3_exercises': (context) => Additional3Exercises(),
+        '/additional_4_exercises': (context) => Additional4Exercises(),
+        '/cheeks_1_exercises': (context) => Cheeks1Exercises(),
+        '/cheeks_2_exercises': (context) => Cheeks2Exercises(),
+        '/cheeks_3_exercises': (context) => Cheeks3Exercises(),
+        '/cheeks_5_exercises': (context) => Cheeks4Exercises(),
+        '/cheeks_4_exercises': (context) => Cheeks5Exercises(),
+        '/lips_1_exercises': (context) => Lips1Exercises(),
+        '/lips_2_exercises': (context) => Lips2Exercises(),
+        '/lips_3_exercises': (context) => Lips3Exercises(),
+        '/lips_4_exercises': (context) => Lips4Exercises(),
+        '/lips_5_exercises': (context) => Lips5Exercises(),
+        '/lips_6_exercises': (context) => Lips6Exercises(),
+        '/lips_7_exercises': (context) => Lips7Exercises(),
+        '/tongue_1_exercises': (context) => Tongue1Exercises(),
+        '/tongue_2_exercises': (context) => Tongue2Exercises(),
+        '/tongue_3_exercises': (context) => Tongue3Exercises(),
+        '/tongue_4_exercises': (context) => Tongue4Exercises(),
+        '/tongue_5_exercises': (context) => Tongue5Exercises(),
+        '/tongue_6_exercises': (context) => Tongue6Exercises(),
+        '/tongue_7_exercises': (context) => Tongue7Exercises(),
       },
     );
   }
